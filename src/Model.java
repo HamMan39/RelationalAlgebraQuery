1a97d9a4458fb04bb8bc6027ce59286a10e5b25b
Owen <opetersen1010@hotmail.com>
2024-01-11 20:09:29 -0500

    Created Model class, added basic constructor

---
+++ src/Model.java
@@ -0,0 +1,12 @@
+public class Model {
+    private String rawText;
+    private View output;
+
+    public Model(View view){
+        output = view;
+    }
+
+    public void submitQuery(){
+
+    }
+}
